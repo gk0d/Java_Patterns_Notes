7078214c5d703c75e2c8d935c9b1c60c1fdab1a0
gk0d <shiwk177@163.com>
2023-03-10 22:56:14 +0800

    Create Animal.java

---
+++ 00_UML类图和时序图/Animal.java
@@ -0,0 +1,14 @@
+public class Animal {
+	public String name;
+	protected boolean isPet;
+	private String state;
+	private int age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
